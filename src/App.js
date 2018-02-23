b5ab1ad810b2fbb3413b84d6ba485f79cdea60d4
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-22 18:52:01 -0800

    Fix things with links in components

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import {
 import Home from './pages/Home';
 import Books from './pages/Books';
 import About from './pages/About';
+import Header from './components/header';
 
 const App = () =>
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/books">Books</Link></li>
-      </ul>
+      <Header />
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/books">Books</Link></li>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'react-router-dom';
+
+const Header = () => 
+    <div>
+        <ul>
+            Hi
+        </ul>
+    </div>
+
+
+export default Header;
