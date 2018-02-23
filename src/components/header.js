a3ac5e6d651f318b89117d4da40cc27be03b6af0
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-02-22 19:21:04 -0800

    Add launch config

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'react-router-dom';
 
-const Header = () => 
+const Header = () => (
     <div>
         <ul>
             Hi
         </ul>
     </div>
-
+);
 
 export default Header;
